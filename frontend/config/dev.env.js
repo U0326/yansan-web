ae3b66f42498073525275966f76addbb0790d4df
Yuu Fujimura <koukokuiranaiyo@gmail.com>
2020-02-20 00:39:36 +0900

    ローカルでDBと接続して起動する為の修正を行った。

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  AJAX_BASE_URL: '"http://localhost:5000"'
+  AJAX_BASE_URL: '"http://0.0.0.0:5000"'
 })
